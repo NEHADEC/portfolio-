8d660210066fac7dabf1e69fab83bf5eed0916f6
NEHADEC <faizanbilal472@gmail.com>
2024-11-10 21:50:42 +0500

    Added HeroSection and ContactForm components with basic styling and functionality

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -11,7 +11,7 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>HTML</li>
         <li>CSS</li>
-        <li>JAVASCIPT</li>
+        <li>JAVASCRIPT</li>
         <li>TYPESCRIPT</li>
         <li>NODE.JS</li>
         <li>NEXT.JS</li>
@@ -22,11 +22,11 @@ const TAB_DATA = [
   },
   {
     title: "Education",
-    id: "eduction",
+    id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Governor initiative </li>
-        <li>collage of khatoon-e-pakistan karachi</li>
+        <li>Governor initiative</li>
+        <li>College of Khatoon-e-Pakistan Karachi</li>
       </ul>
     )
   },
@@ -35,15 +35,12 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Frountend Developer</li>
-        <li>AWS professional cloud Developer</li>
-        
+        <li>Frontend Developer</li>
+        <li>AWS Professional Cloud Developer</li>
       </ul>
     )
   }
-
-]
-
+];
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
@@ -56,9 +53,8 @@ const AboutSection = () => {
   };
 
   return (
-    <section id='about'className="text-white">
-      {" "}
-      <div className="md:grid md:grid-cols-2 gap-8 items-centerpy-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+    <section id="about" className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src="/images/About.jpg"
           alt="about image"
@@ -68,37 +64,38 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            "I am a passionate Frontend Developer with a strong foundation in
+            &quot;I am a passionate Frontend Developer with a strong foundation in
             UI/UX design, web development, and game development. With a keen eye
             for detail and user-centric design, I aim to create innovative and
             responsive websites that offer seamless user experiences. Whether
-            it's developing a visually appealing interface, building interactive
+            it&apos;s developing a visually appealing interface, building interactive
             web applications, or crafting immersive games, I combine my
             technical skills with creativity to deliver solutions that are both
-            functional and engaging."
+            functional and engaging.&quot;
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}>
-                {" "}
-              Skills{" "}
+              active={tab === "skills"}
+            >
+              Skills
             </TabButton>
             <TabButton
-              selectTab={() => handleTabChange("eduction")}
-              active={tab === "education"}>
-                {" "}
-              Education{" "}
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}>
-                {" "}
-              Certifications{" "}
+              active={tab === "certifications"}
+            >
+              Certifications
             </TabButton>
-           
           </div>
-          <div className="mt-8">{TAB_DATA.find((t) => t.id === tab). content}</div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content}
+          </div>
         </div>
       </div>
     </section>
--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useState, useEffect } from 'react';
@@ -14,7 +13,7 @@ const ContactForm = () => {
   const [submitted, setSubmitted] = useState(false);
 
   useEffect(() => {
-    setIsClient(true); // Only runs on client side
+    setIsClient(true); // Runs only on the client side
   }, []);
 
   const validateForm = () => {
@@ -43,63 +42,63 @@ const ContactForm = () => {
   if (!isClient) return null; // Prevents SSR
 
   return (
-    <section id="ContactForm" className='grid md:grid-cols-2 my-12 py-24 gap-4 relative'>
-      <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 left-0 transform -translate-x-1/2 -translate-y-1/2'></div>
-      <div className='z-10'>
-        <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
-        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+    <section id="ContactForm" className="grid md:grid-cols-2 my-12 py-24 gap-4 relative">
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 left-0 transform -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="z-10">
+        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <p className="text-[#ADB7BE] mb-4 max-w-md">
           I'm currently looking for new opportunities. My inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
         </p>
-        <div className='socials flex gap-2'>
+        <div className="socials flex gap-2">
           <Link href="https://github.com/NEHADEC" target="_blank">
-            <Image src={GithubIcon} alt='GitHub Icon' width={30} height={30} />
+            <Image src={GithubIcon} alt="GitHub Icon" width={30} height={30} />
           </Link>
           <Link href="https://www.linkedin.com/in/neha-bilal-7abab22b4?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bg%2B7SDXZJTCmeJtf2cOGMTQ%3D%3D" target="_blank">
-            <Image src={LinkedinIcon} alt='LinkedIn Icon' width={30} height={30} />
+            <Image src={LinkedinIcon} alt="LinkedIn Icon" width={30} height={30} />
           </Link>
         </div>
       </div>
       <div>
-        <form className='flex flex-col' onSubmit={handleSubmit}>
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           {submitted && <p className="text-green-500 mb-4">Message sent successfully!</p>}
-          <div className='mb-6'>
-            <label htmlFor='email' className='text-white block mb-2 text-sm font-medium'>Your email</label>
-            <input 
-              type='email'
-              id='email'
+          <div className="mb-6">
+            <label htmlFor="email" className="text-white block mb-2 text-sm font-medium">Your email</label>
+            <input
+              type="email"
+              id="email"
               value={formData.email}
               onChange={handleChange}
-              className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-              placeholder='Email'
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Email"
             />
             {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
           </div>
-          <div className='mb-6'>
-            <label htmlFor='subject' className='text-white block mb-2 text-sm font-medium'>Subject</label>
-            <input 
-              type='text'
-              id='subject'
+          <div className="mb-6">
+            <label htmlFor="subject" className="text-white block mb-2 text-sm font-medium">Subject</label>
+            <input
+              type="text"
+              id="subject"
               value={formData.subject}
               onChange={handleChange}
-              className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
-              placeholder='Just saying hi'
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Just saying hi"
             />
             {errors.subject && <p className="text-red-500 text-sm">{errors.subject}</p>}
           </div>
-          <div className='mb-6'>
-            <label htmlFor='message' className='text-white block mb-2 text-sm font-medium'>Message</label>
+          <div className="mb-6">
+            <label htmlFor="message" className="text-white block mb-2 text-sm font-medium">Message</label>
             <textarea
-              id='message'
+              id="message"
               value={formData.message}
               onChange={handleChange}
-              className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
               placeholder="Let's talk about..."
             />
             {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
           </div>
           <button
-            type='submit'
-            className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
           >
             Send Message
           </button>
